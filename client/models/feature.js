79713f8207aa3a7d0882d8c01592d012678958eb
Fernando Cano <fernandocano@winduponthewater.com>
2017-07-07 23:49:19 -0500

    CMS backed pages.

--- client/models/feature.js
+++ client/models/feature.js
@@ -2,37 +2,25 @@ var AmpersandModel = require('ampersand-model');
 var crypto = require('crypto');
 
 function stringHash(data) {
-   return crypto.createHash(crypto.getHashes()[0]).update(data).digest('base64');
+	return crypto.createHash(crypto.getHashes()[0]).update(data).digest('base64');
 }
 
 module.exports = AmpersandModel.extend({
-    props: {
-        id: 'any',
-        title: ['string', true, ''],
-        content: ['string', true, ''],
-        badges: ['array', true, function () { return [];}]
-    },
-    session: {
-        selected: ['boolean', true, false]
-    },
-    derived: {
-        hash: {
-            deps: ['title', 'content'],
-            fn: function () {
-                return stringHash(this.title + ' ' + this.content);
-            }
-        },
-        editUrl: {
-            deps: ['id'],
-            fn: function () {
-                return '/feature/' + this.id + '/edit';
-            }
-        },
-        viewUrl: {
-            deps: ['id'],
-            fn: function () {
-                return '/feature/' + this.id;
-            }
-        }
-    }
+	props: {
+		id: 'any',
+		title: ['string', true, ''],
+		content: ['string', true, ''],
+		badges: ['array', true, function () { return [];}]
+	},
+	session: {
+		selected: ['boolean', true, false]
+	},
+	derived: {
+		hash: {
+			deps: ['title', 'content'],
+			fn: function () {
+				return stringHash(this.title + ' ' + this.content);
+			}
+		}
+	}
 });
--- client/pages/features.js
+++ client/pages/features.js
@@ -1,22 +1,28 @@
+var AmpersandCollection = require('ampersand-collection');
 var PageView = require('./base');
 var templates = require('../templates');
 var FeatureView = require('../views/feature');
+var Feature = require('../models/feature');
+var _ = require('lodash');
 
 
 module.exports = PageView.extend({
-    pageTitle: 'muk Features',
-    template: templates.pages.features,
-    events: {
-    },
-    render: function () {
-        this.renderWithTemplate();
-        this.renderCollection(this.collection, FeatureView, this.queryByHook('feature-list'));
-        if (!this.collection.length || this.collection.length == 0) {
-            this.fetchCollection();
-        }
-    },
-    fetchCollection: function () {
-        this.collection.fetch();
-        return false;
-    }
+	pageTitle: 'muk Features',
+	template: templates.pages.features,
+	cmsId: 'e363fa5156a34be490f8be7c4507ad3f',
+	events: {
+	},
+	bindings: _.extend({}, PageView.prototype.bindings, {
+		'model.cms.page.a': {type: 'text', hook: 'outl-a'}
+	}),
+	render: function () {
+		if (!this.collection) {
+			this.fetchCollection();
+		}
+		this.renderWithTemplate();
+		this.renderCollection(this.collection, FeatureView, this.queryByHook('feature-list'));
+	},
+	fetchCollection: function () {
+		this.collection =  new AmpersandCollection(this.model.cms.page.b, {model: Feature});
+	}
 });
